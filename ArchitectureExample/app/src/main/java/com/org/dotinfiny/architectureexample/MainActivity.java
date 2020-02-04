d9bc356de7684c1b54f032b95b490518bf4ca60c
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2020-02-04 14:13:49 +0500

    Done Adapter and ViewHolder work in App using Room SQLITE and Repository Process using ViewHolder

--- ArchitectureExample/app/src/main/java/com/org/dotinfiny/architectureexample/MainActivity.java
+++ ArchitectureExample/app/src/main/java/com/org/dotinfiny/architectureexample/MainActivity.java
@@ -3,10 +3,13 @@ package com.org.dotinfiny.architectureexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.org.dotinfiny.architectureexample.Adapter.NoteAdapter;
 import com.org.dotinfiny.architectureexample.DatabaseOperation.Note;
 import com.org.dotinfiny.architectureexample.ProjectViewModel.NoteViewModel;
 
@@ -15,17 +18,26 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private NoteViewModel noteViewModel;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+        recyclerView.setHasFixedSize(true);
+
+        noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
         noteViewModel = ViewModelProviders.of(this).
                 get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                Toast.makeText(MainActivity.this, "Update RecyclerView, onChanged", Toast.LENGTH_SHORT).show();
+                noteAdapter.setNotes(notes);
+//                Toast.makeText(MainActivity.this, "Update RecyclerView, onChanged", Toast.LENGTH_SHORT).show();
             }
         });
 
